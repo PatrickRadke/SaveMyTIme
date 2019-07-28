11586cfeeb0c685d46511e2fac60b289e7288367
Patrick Radke <parad@gmx.net>
2019-07-28 22:55:57 +0200

    Integration of dynamically changing Timer via Binding

---
+++ SaveMyTime/Helper/DateTimeHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveMyTime.Helper
+{
+    static class DateTimeHelper
+    {
+        public static string GetCurrentDateTime() => DateTime.Now.ToString();
+    }
+}
--- SaveMyTime/Viewmodel/MainWindowViewModel.cs
+++ SaveMyTime/Viewmodel/MainWindowViewModel.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using SaveMyTime.Helper;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SaveMyTime.Viewmodel
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : INotifyPropertyChanged
     {
+        private string currentTime;
+
+        public string CurrentTime
+        {
+            get { return currentTime; }
+            set
+            {
+                currentTime = value;
+                NotifyPropertychanged("CurrentTime");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertychanged(String propertyname) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+
+        public MainWindowViewModel()
+        {
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.DoWork += GetTime;
+            bw.RunWorkerAsync();
+        }
+
+        /// <summary>
+        /// Starts the endless loop for 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GetTime(object sender, DoWorkEventArgs e)
+        {
+            while(true)
+            {
+                CurrentTime = DateTimeHelper.GetCurrentDateTime();
+                Thread.Sleep(1000);
+            }
+        }
     }
 }
